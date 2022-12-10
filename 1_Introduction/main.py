e930e22b0f011b8beaa834dbc4cefb1b6801427a
JonMS95 <jon_portugalete@hotmail.com>
2022-12-10 18:32:29 +0100

    Commented the line where sale price series is taken from the test data (that column does not exist).

--- 1_Introduction/main.py
+++ 1_Introduction/main.py
@@ -11,7 +11,9 @@ test_data  = pd.read_csv(PATH_TEST_CSV, index_col = 'Id')
 
 # Obtain target ('y') and features/predictors ('X').
 y = train_data['SalePrice']
-y_test = test_data['SalePrice']
+# y_test = test_data['SalePrice']
+#If we print the test data columns, we will find no sale price series.
+print(test_data.columns)
 
 # Trivia fact: 'y' variable could have been declared as y = train_data.SalePrice as well. However, declaring it as it's done above is strongly recommended, because
 # it covers a more general case. If the variable name has special characters within it, then the dot ('.') method, it's to say, accessing the variable as a member
@@ -24,3 +26,5 @@ X_test = test_data[features]
 # Until this point, y and X variables have been recorded for the train and test cases. Note that testing is not considered to be the same as validation.
 # In fact, the next line is going to break y and X variables into training and validation (not testing) data chunks.
 X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size = SIZE_TRAIN, test_size = SIZE_VAL, random_state = SPLIT_RANDOM_STATE)
+
+print(X_train[0:5])
