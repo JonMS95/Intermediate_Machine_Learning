5ca7227d6e17acfe429d5626708e0fa1a84145f9
JonMS95 <jon_portugalete@hotmail.com>
2022-12-10 18:18:39 +0100

    Added paths to csv file as well as train and validation size proportions.

---
+++ 1_Introduction/constants.py
@@ -0,0 +1,5 @@
+PATH_TRAIN_CSV  = "/home/jon/Desktop/scripts/ML/Intermediate_Machine_Learning/Data_sets/train.csv"
+PATH_TEST_CSV   = "/home/jon/Desktop/scripts/ML/Intermediate_Machine_Learning/Data_sets/test.csv"
+
+SIZE_TRAIN  = 0.8
+SIZE_VAL    = 0.2
