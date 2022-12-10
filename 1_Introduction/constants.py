5b76ede57cc05d61005df57390ab5b1e4aaeacaa
JonMS95 <jon_portugalete@hotmail.com>
2022-12-10 19:19:39 +0100

    Added path to submission csv file.

--- 1_Introduction/constants.py
+++ 1_Introduction/constants.py
@@ -1,5 +1,6 @@
 PATH_TRAIN_CSV  = "/home/jon/Desktop/scripts/ML/Intermediate_Machine_Learning/Data_sets/train.csv"
 PATH_TEST_CSV   = "/home/jon/Desktop/scripts/ML/Intermediate_Machine_Learning/Data_sets/test.csv"
+PATH_SUBMISSION = "/home/jon/Desktop/scripts/ML/Intermediate_Machine_Learning/Submission_CSV_files/submission.csv"
 
 SIZE_TRAIN  = 0.8
 SIZE_VAL    = 0.2
