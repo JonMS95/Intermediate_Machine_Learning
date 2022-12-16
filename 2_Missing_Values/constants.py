7ea86fba58d8728f7e2365f1c9da55db24ece59c
JonMS95 <jon_portugalete@hotmail.com>
2022-12-16 18:42:20 +0100

    Some constants were added.

--- 2_Missing_Values/constants.py
+++ 2_Missing_Values/constants.py
@@ -1,1 +1,7 @@
 PATH_MELBOURNE_CSV  = "/home/jon/Desktop/scripts/ML/Intermediate_Machine_Learning/Data_sets/melb_data.csv"
+
+SIZE_TRAIN  = 0.8
+SIZE_VAL    = 0.2
+
+SPLIT_RANDOM_STATE  = 0
+RFR_RANDOM_STATE    = 0
